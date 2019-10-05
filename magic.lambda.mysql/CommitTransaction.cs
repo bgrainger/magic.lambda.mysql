ede6f81e7bead94a5b8e0d4088fc4d28c7857b65
Thomas Hansen <thomas@gaiasoul.com>
2019-10-05 16:02:08 +0300

    Database transaction support

---
+++ magic.lambda.mysql/CommitTransaction.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using magic.node;
+using magic.signals.contracts;
+using magic.lambda.mysql.utilities;
+
+namespace magic.lambda.mysql
+{
+    /// <summary>
+    /// [mysql.transaction.commit] slot for committing the top level MySQL
+    /// database transaction.
+    /// </summary>
+	[Slot(Name = "mysql.transaction.commit")]
+	public class CommitTransaction : ISlot
+	{
+        /// <summary>
+        /// Handles the signal for the class.
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal the slot.</param>
+        /// <param name="input">Root node for invocation.</param>
+		public void Signal(ISignaler signaler, Node input)
+		{
+            signaler.Peek<Transaction>("mysql.connect").Commit();
+        }
+	}
+}
---
+++ magic.lambda.mysql/utilities/Transaction.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
+ * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ */
+
+using System;
+using MySql.Data.MySqlClient;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.mysql.utilities
+{
+    /*
+     * Helper class wrapping a transaction, creating a guarantee of that the
+     * transaction is rolled back, unless it's explicitly committed.
+     */
+    internal class Transaction : IDisposable
+    {
+        readonly MySqlTransaction _transaction;
+        bool _signaled;
+
+        /*
+         * Creates a new instance of your type.
+         */
+        public Transaction(ISignaler signaler)
+        {
+            _transaction = signaler.Peek<MySqlConnection>("mysql.connect").BeginTransaction();
+        }
+
+        /*
+         * Rolls back the transaction.
+         */
+        public void Rollback()
+        {
+            _signaled = true;
+            _transaction.Rollback();
+        }
+
+        /*
+         * Committing your transaction.
+         */
+        public void Commit()
+        {
+            _signaled = true;
+            _transaction.Commit();
+        }
+
+        public void Dispose()
+        {
+            if (!_signaled)
+                _transaction.Rollback();
+        }
+    }
+}
