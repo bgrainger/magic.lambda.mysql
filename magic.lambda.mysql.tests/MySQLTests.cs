032b07e9a68fdbb7936fe9c64b8a3ca3f43a93a3
Thomas Hansen <thomas@gaiasoul.com>
2019-10-23 14:11:47 +0300

    Update MySQLTests.cs

--- magic.lambda.mysql.tests/MySQLTests.cs
+++ magic.lambda.mysql.tests/MySQLTests.cs
@@ -171,12 +171,32 @@ namespace magic.lambda.mysql.tests
    table:SomeTable
    where
       and
-         foo.like:query%
-   ");
-            Assert.Single(lambda.Children.First().Children);
-            Assert.Equal("select * from `SomeTable` where (`foo` like @0) limit 25", lambda.Children.First().Value);
+         foo1.like:query%
+         foo2.mt:int:5
+         foo3.lt:int:5
+         foo4.mteq:int:5
+         foo5.lteq:int:5
+         foo6.neq:int:5
+         foo7.eq:int:5
+         .foo.like:query%");
+            Assert.Equal(8, lambda.Children.First().Children.Count());
+            Assert.Equal("select * from `SomeTable` where (`foo1` like @0 and `foo2` > @1 and `foo3` < @2 and `foo4` >= @3 and `foo5` <= @4 and `foo6` != @5 and `foo7` = @6 and `foo.like` = @7) limit 25", lambda.Children.First().Value);
             Assert.Equal("@0", lambda.Children.First().Children.First().Name);
             Assert.Equal("query%", lambda.Children.First().Children.First().Value);
+            Assert.Equal("@1", lambda.Children.First().Children.Skip(1).First().Name);
+            Assert.Equal(5, lambda.Children.First().Children.Skip(1).First().Value);
+            Assert.Equal("@2", lambda.Children.First().Children.Skip(2).First().Name);
+            Assert.Equal(5, lambda.Children.First().Children.Skip(2).First().Value);
+            Assert.Equal("@3", lambda.Children.First().Children.Skip(3).First().Name);
+            Assert.Equal(5, lambda.Children.First().Children.Skip(3).First().Value);
+            Assert.Equal("@4", lambda.Children.First().Children.Skip(4).First().Name);
+            Assert.Equal(5, lambda.Children.First().Children.Skip(4).First().Value);
+            Assert.Equal("@5", lambda.Children.First().Children.Skip(5).First().Name);
+            Assert.Equal(5, lambda.Children.First().Children.Skip(5).First().Value);
+            Assert.Equal("@6", lambda.Children.First().Children.Skip(6).First().Name);
+            Assert.Equal(5, lambda.Children.First().Children.Skip(6).First().Value);
+            Assert.Equal("@7", lambda.Children.First().Children.Skip(7).First().Name);
+            Assert.Equal("query%", lambda.Children.First().Children.Skip(7).First().Value);
         }
 
         [Fact]
