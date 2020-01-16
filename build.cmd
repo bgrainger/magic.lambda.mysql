bdf0c6d24862399a24b2c7d5bb8c8344d784bc51
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:26:00 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,8 @@
+
+set version=%1
+set key=%2
+
 cd %~dp0
+
 dotnet build magic.lambda.mysql/magic.lambda.mysql.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet nuget push magic.lambda.mysql/bin/Release/magic.lambda.mysql.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
